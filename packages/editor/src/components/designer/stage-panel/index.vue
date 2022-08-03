8b9b127e41ab7b227736bc6a479a970007069924
zhangxin0214 <zhangxin0214@tal.com>
2022-08-03 18:02:53 +0800

    优化默认文案提示

--- packages/editor/src/components/designer/stage-panel/index.vue
+++ packages/editor/src/components/designer/stage-panel/index.vue
@@ -8,6 +8,9 @@
         <div class="stage-hint">
             请从左侧选择一个组件，拖拽或双击添加至此处
         </div>
+        <div class="stage-form">
+          
+        </div>
     </div>
 </template>
 <script>
@@ -23,11 +26,17 @@ export default {
 <style lang="scss" scoped>
 .stage{
   &-container{
-    
+    width: 100%;
+    height: 100%;
     padding: 10px;
     background: #f1f2f3;
     overflow-x: hidden;
     overflow-y: auto;
+  }
+  &-hint{
+    text-align: center;
+    color: #444;
+    margin-top:40%;
   }
 }
 </style>
