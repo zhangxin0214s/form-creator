28dc25e1a1d7313a5232b97768fec9e91c911439
zhangxin0214 <zhangxin0214@tal.com>
2022-08-03 18:06:43 +0800

    属性面板代码初版

--- packages/editor/src/components/designer/settting-panel/index.vue
+++ packages/editor/src/components/designer/settting-panel/index.vue
@@ -3,3 +3,21 @@
  * email: zhangxin0214@tal.com
  * date: 2022.08.01
  */
+ <template>
+    <div class="setting-container">
+        
+    </div>
+</template>
+<script>
+export default {
+  name: "SettingPanel",
+  data(){
+    return {
+        
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
