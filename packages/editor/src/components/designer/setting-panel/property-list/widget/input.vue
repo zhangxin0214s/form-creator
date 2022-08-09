dcdf1c5485f717af8c866551514e0074b93af4f9
zhangxin0214s <15110272646@163.com>
2022-08-10 00:06:11 +0800

    完成属性面板基本组件渲染

--- packages/editor/src/components/designer/setting-panel/property-list/widget/input.vue
+++ packages/editor/src/components/designer/setting-panel/property-list/widget/input.vue
@@ -2,11 +2,11 @@
     <div class="input-container">
         <el-collapse v-model="activeNames" class="input-collapse">
             <el-collapse-item name="1" title="基础属性">
-                <template v-for="{value,key,index} in selectedWidgetProp.basicProp" :key="index">
+                <div v-for="(item,key,index) in selectedWidgetProp.basicProp" :key="index">
                     <component  
                         :is="componentMap[getPropCompName(key)]"
                     ></component>
-                </template>
+                </div>
             </el-collapse-item>
 
             <el-collapse-item name="2" title="高级属性">
@@ -19,7 +19,7 @@
 import { ref } from "vue"
 import { defineProps } from 'vue'
 import { BASCI_COMPONENTS, BASIC_PROPERTIES} from '../propertyRegister'
-import * as basicComponents from './components/index';
+import * as basicComponents from '../components/index';
 
 const selectedWidgetProp = defineProps([
     'selectedWidget',
@@ -30,7 +30,7 @@ const componentMap = {
   ...basicComponents
 }
 const activeNames = ref(['1', '2', '3'])
-const a = ref({name:'input'})
+const a = ref({name:'input1'})
 const getPropCompName = (key) =>{
     console.log('key:',key)
     return BASCI_COMPONENTS[BASIC_PROPERTIES[key]]
