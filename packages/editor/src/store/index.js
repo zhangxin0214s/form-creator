7c1656b4fcf264a9223316f5f45f64a435593cd9
zhangxin0214 <zhangxin0214@tal.com>
2022-08-09 11:52:59 +0800

    opt code

--- packages/editor/src/store/index.js
+++ packages/editor/src/store/index.js
@@ -3,7 +3,7 @@ import { defineStore } from 'pinia'
 export const widgetStore = defineStore('widget', {
     state: () =>{
         return {
-            widgetList: [], // 舞台上组件列表
+            widgetList: [], // 舞台组件列表
             selectedWidget:null // 当前选中组件
         }
     },
