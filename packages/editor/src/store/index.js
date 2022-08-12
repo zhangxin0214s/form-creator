d9087bfca3a99390777522e6fa5ef676bf8833eb
zhangxin0214s <15110272646@163.com>
2022-08-12 23:44:48 +0800

    处理冲突

--- packages/editor/src/store/index.js
+++ packages/editor/src/store/index.js
@@ -4,7 +4,8 @@ export const widgetStore = defineStore('widget', {
     state: () =>{
         return {
             widgetList: [], // 舞台组件列表
-            selectedWidget:null // 当前选中组件
+            selectedWidget:null, // 当前选中组件
+            maskList:[]//遮罩组件列表
         }
     },
     actions: {},
