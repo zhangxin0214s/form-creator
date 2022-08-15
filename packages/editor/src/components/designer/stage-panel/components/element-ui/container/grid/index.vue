ba15ccab62b93538a96386baa80b15d0799f3fa8
zhangxin0214 <zhangxin0214@tal.com>
2022-08-15 16:05:15 +0800

    优化遮罩

--- packages/editor/src/components/designer/stage-panel/components/element-ui/container/grid/index.vue
+++ packages/editor/src/components/designer/stage-panel/components/element-ui/container/grid/index.vue
@@ -1,22 +1,35 @@
 <template>
-    <el-row class="grid-container">
-        <template v-for="(colWidget, colIdx) in widget.options.advanced.cols" :key="colIdx">
-            <Col 
-                :colWidget="colWidget"
-            >
-            </Col>
-        </template>  
+  <container-mask :widget="widget">
+    <el-row class="grid-container" :class="[selectedWidget?.id === widget?.id?'select':'']">
+      <template v-for="(colWidget, colIdx) in widget.options.advanced.cols" :key="colIdx">
+        <Col 
+          :colWidget="colWidget"
+        >
+        </Col>
+    </template>  
     </el-row>
+  </container-mask>
+  
 </template>
 <script setup>
-    import Col from "./grid-col.vue"
-    defineProps([
-        'widget'
-    ])
+  import { widgetStore } from '@/store/index';
+  import { storeToRefs } from 'pinia';
+  import containerMask from "../../common/containerMask.vue"
+  import Col from "./grid-col.vue"
+  const _widgetStore = widgetStore();
+  const { selectedWidget} = storeToRefs(_widgetStore);
+  defineProps([
+      'widget'
+  ])
+
 </script>
  <style lang="scss" scoped>
  .grid-container{
-    border:1px dashed #444;
+    padding:1px;
+    outline:1px dashed #444;
+ }
+ .select {
+    outline:1px dashed $--color-primary;
  }
  .el-row {
   margin-bottom: 20px;
