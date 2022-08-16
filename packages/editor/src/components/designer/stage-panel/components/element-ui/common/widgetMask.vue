5f993ff75ae72ce2c0e10bfd1a55fa2aaa377c88
zhangxin0214 <zhangxin0214@tal.com>
2022-08-16 17:38:25 +0800

    抽离表单公共配置

--- packages/editor/src/components/designer/stage-panel/components/element-ui/common/widgetMask.vue
+++ packages/editor/src/components/designer/stage-panel/components/element-ui/common/widgetMask.vue
@@ -3,26 +3,14 @@
 		class="container-mask"
 		@click.stop="selected(widget)"
 	>
-		<el-form
-			:label-position="basicProp.labelAlign.value"
-			:label-width="basicProp.labelWidth.value+'px'"
-			:model="widget.options.basic[inputKey]"
-			:rules="{
-				value:[
-					{
-						required:props.basicProp.required.value
-					}
-				]
-			}"
+		<el-form-item
+			:class="[selectedWidget?.id === widget?.id?'select':'']"
+			:label="basicProp.label.value"
+			prop="value"
 		>
-			<el-form-item
-				:class="[selectedWidget?.id === widget?.id?'select':'']"
-				:label="basicProp.label.value"
-				prop="value"
-			>
-				<slot></slot>
-			</el-form-item>
-		</el-form>
+			<slot></slot>
+		</el-form-item>
+		
 		<!-- <div class="container-mask-title" v-if="selectedWidget?.id === widget?.id">
             <span class="text">
                 {{ widget.options.basic.name.value }}
