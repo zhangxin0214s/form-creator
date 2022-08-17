dac9dd06017ebda86bbe3e12956cbb5a0a7f3205
jam <18611748482@163.com>
2022-08-17 17:48:58 +0800

    [feat]set icon

--- packages/editor/src/components/designer/setting-panel/property-list/components/private/addSelectList.vue
+++ packages/editor/src/components/designer/setting-panel/property-list/components/private/addSelectList.vue
@@ -8,26 +8,31 @@
 		<el-form-item
 			v-if="advancedProp.selectConfig.label"
 			:label="`${advancedProp.selectConfig.label}:`"
-      label-width="100px"
-		>
+      label-width="100px">
       <div class="" v-for="item in LIST.lists">
         <el-input
-  				v-model="item.label"
-  				:placeholder="item.label"
-  			/>
-        <el-button size="small" type="primary" circle :icon="Delete" @click.native="deleteList(item.value)" />
+          v-model="item.label"
+          :placeholder="item.label"
+        >
+          <template #prepend>
+            <el-button :icon="Plus" @click.native="addDetail(item.value)" />
+          </template>
+          <template #append>
+            <el-button :icon="Delete" @click.native="deleteList(item.value)" />
+          </template>
+        </el-input>
       </div>
 		</el-form-item>
 
     <el-form-item @click.native="addList">
-      <svg-icon icon-class="add"/>
+      <el-icon><Plus /></el-icon>
       <span>添加选项</span>
     </el-form-item>
 	</el-form>
 </template>
 
 <script setup>
-  import { Delete } from '@element-plus/icons-vue'
+  import { Delete, Plus, CirclePlusFilled } from '@element-plus/icons-vue'
   const PROPS = defineProps([
     'advancedProp'
   ])
@@ -41,11 +46,24 @@
       disabled: false, // 当前舞台列表是否禁用
     })
   }
+  const addDetail = (id) => {
+    let {lists} = LIST;
+    // lists.push({
+    //   label: `新选项${lists.length}`,
+    //   value: lists.length,
+    //   checked: true, // 列表是否展示
+    //   disabled: false, // 当前舞台列表是否禁用
+    // })
+  }
   const deleteList = (id) => {
     let {lists} = LIST,
         delIndex = lists.findIndex(option=>option.value===id);
     lists.splice(delIndex, 1);
     lists.forEach((child,index) => child.value=index)
+  }
+
+  const test = () => {
+    console.log(111);
   }
 
 </script>
