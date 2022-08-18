0be908281bb96bdf642bae226b2235c1cba24b01
huzezhou <2697872669@qq.com>
2022-08-18 11:28:48 +0800

    增加卡片容器

---
+++ packages/editor/src/components/designer/stage-panel/components/element-ui/container/card/card.vue
@@ -0,0 +1,13 @@
+<template>
+<div>卡片</div>
+</template>
+
+<script>
+export default {
+  name:"card"
+}
+</script>
+
+<style scoped>
+
+</style>
--- packages/editor/src/components/designer/stage-panel/components/element-ui/container/index.js
+++ packages/editor/src/components/designer/stage-panel/components/element-ui/container/index.js
@@ -1,8 +1,10 @@
 import grid from './grid/index.vue'
 import tabs from './tabs/index.vue'
 import Table from './table.vue'
+import card from './card/card'
 export {
     grid,
     tabs,
-    Table
+    Table,
+    card
 }
