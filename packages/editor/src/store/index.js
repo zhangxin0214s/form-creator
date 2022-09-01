420e1ad098fb6ef096f5d1504d92abc7cf7ec058
v_zhangrui2 <v_zhangrui2@tal.com>
2022-09-01 16:37:13 +0800

    [feat]reset

--- packages/editor/src/store/index.js
+++ packages/editor/src/store/index.js
@@ -29,24 +29,23 @@ export const widgetStore = defineStore('widget', {
          * 复制组件
          * @param {*} target
          */
-        copyWidget(){
-          if (!this.selectedWidget) return;
-          let newOrigin = deepClone(this.selectedWidget);
-          newOrigin.id = generateId();
-          // 处理栅格 && 标签页内组件
-          if(newOrigin.type === 'grid' || newOrigin.type === 'tabs'){
-              const cols = newOrigin.options.advanced.cols;
-              cols.forEach(col =>{
-                  col.widgetList.forEach(widget =>{
-                      if(widget){
-                          widget.id = generateId();
-                      }
-                  })
-              })
-          }
-          this.widgetList.push(newOrigin);
-          this.recordHistory();
-        },
+         copyWidget(target){
+             let newOrigin = deepClone(target);
+             newOrigin.id = generateId();
+
+             // 处理栅格 && 标签页内组件
+             if(newOrigin.type === 'grid' || newOrigin.type === 'tabs'){
+                 const cols = newOrigin.options.advanced.cols;
+                 cols.forEach(col =>{
+                     col.widgetList.forEach(widget =>{
+                         if(widget){
+                             widget.id = generateId();
+                         }
+                     })
+                 })
+             }
+             this.widgetList.push(newOrigin);
+         },
         /**
          * 删除组件
          * @param {*} target
