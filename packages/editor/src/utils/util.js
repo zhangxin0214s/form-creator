c0a07ee2c63845bf592c4a1128276e0f4085a03b
v_zhangrui2 <v_zhangrui2@tal.com>
2022-09-06 20:14:59 +0800

    [feat]data

--- packages/editor/src/utils/util.js
+++ packages/editor/src/utils/util.js
@@ -1,4 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
+import { ElMessage } from 'element-plus';
+import { toRaw } from '@vue/reactivity'
+
 /**
  * 生成唯一ID
  */
@@ -34,4 +37,80 @@ export function deepClone(obj){
     }
     return result;
 
+}
+
+function judgeSameKey (lists, keyArray=[]) {
+  // 同级key不可相同
+  let same = false;
+  lists.some(list => {
+    const key = list.options.basic.ruleFormKey.value;
+    if (keyArray.indexOf(key) > -1) {
+      same = list;
+      return true;
+    } else {
+      keyArray.push(key);
+    }
+  })
+  return same;
+}
+
+function judgeEmptyKey (lists) {
+  let emptyTarget;
+  lists.some((list,index) => {
+    const key = list.options.basic.ruleFormKey.value;
+    if (key.trim() === "") {
+      // key为空
+      emptyTarget = list;
+      return true;
+    }
+  })
+  return emptyTarget;
+}
+
+function linkAgeDeep (lists, linkAge) {
+  let emptyTarget = judgeEmptyKey(lists.value || lists),
+      same = judgeSameKey(lists.value || lists),
+      target;
+  if (emptyTarget) {
+    target = emptyTarget;
+    ElMessage({
+      showClose: true,
+      message: '该组件参数key为空',
+      center: true,
+    })
+  } else if (same) {
+    target = same;
+    ElMessage({
+      showClose: true,
+      message: `参数${target.options.basic.ruleFormKey.value}有多个`,
+      center: true,
+    })
+  } else {
+    let array = lists.value || lists;
+    array.forEach(list => {
+      const {category} = list;
+      const key = list.options.basic.ruleFormKey.value;
+      if (category === 'container') {
+        linkAge[key] = {};
+        list.options.advanced.cols.forEach(list => {
+          if (list.ruleFormKey) {
+            // 针对标签页面
+            linkAge[key][list.ruleFormKey.value] = {};
+          }
+          if (list.widgetList.length>0) {
+            linkAgeDeep(list.widgetList, list.ruleFormKey?linkAge[key][list.ruleFormKey.value]:linkAge[key]);
+          } else {
+            // 空容器列表
+          }
+        })
+      } else {
+        linkAge[key] = list.value;
+      }
+    })
+  }
+  return target;
+}
+
+export function handleData (widgetList, linkAge) {
+  return linkAgeDeep(widgetList, linkAge);
 }
