3c4412d2c01fa6ed8e78bd8e1b4a69df5fce92bc
huzezhou <2697872669@qq.com>
2022-09-09 16:30:33 +0800

    修改个别组件不显示问题

--- packages/render/element-plus/packages/dateTimeRange/index.js
+++
@@ -1,8 +0,0 @@
-import dateTimeRange from './src/dateTimeRange.vue';
-
-
-dateTimeRange.install = (App) => {
-	App.component(dateTimeRange.__name, dateTimeRange);
-};
-
-export default dateTimeRange;
