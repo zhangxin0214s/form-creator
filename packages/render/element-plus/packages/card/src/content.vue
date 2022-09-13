0eefc8d0e14c99b9371a5e391195484d917451a7
zhangxin0214 <zhangxin0214@tal.com>
2022-09-13 17:31:04 +0800

    opt code

--- packages/render/element-plus/packages/card/src/content.vue
+++ packages/render/element-plus/packages/card/src/content.vue
@@ -4,35 +4,6 @@
     </el-col>
 </template>
 <script setup>
-    import { inject } from 'vue'
-    const props = defineProps([
-        'widgetList',
-        'widget',
-        'propKey',
-        'ruleForm',
-        'selectedWidget',
-        'isEditor'
-    ])
-
-    const onEnd = inject("onEnd")
-    const onEnd1 = () =>{
-        onEnd()
-    }
-    
-    const selected = inject("selected")
-    const selected1 = (element) =>{
-        selected(element)
-    }
-
-    const getPropKey = (element,index) =>{
-        if(props.propKey) {
-            if(props.widget.ruleFormKeyType === 'object'){
-                return `${props.propKey}.${element.ruleFormKey}`
-            }
-        }else{
-            return `${element.ruleFormKey}`
-        }
-    }
 </script>
  <style lang="scss" scoped>
  .grid-content1{
--- packages/render/element-plus/packages/tabs/src/content.vue
+++ packages/render/element-plus/packages/tabs/src/content.vue
@@ -4,34 +4,6 @@
     </el-col>
 </template>
 <script setup>
-    import { inject } from 'vue'
-    const props = defineProps([
-        'colWidget',
-        'ruleForm',
-        'widget',
-        'propKey',
-        'colIdx',
-        'isEditor',
-        'selectedWidget'
-    ])
-    const getPropKey = (element,index) =>{
-        if(props.propKey) {
-            return `${props.propKey}.${index}.${element.ruleFormKey}`
-        }else{
-            return `${element.ruleFormKey}`
-        }
-    }
-
-    const onEnd = inject('onEnd')
-    const onEnd1 = () =>{
-        onEnd()
-    }
-    
-    const selected = inject('selected')
-    const selected1 = (element) =>{
-        selected(element)
-    }
-
 </script>
  <style lang="scss" scoped>
  .grid-content1{
