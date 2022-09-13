e733e154c49adbb3aa6cf859085a75bf17aaf1ca
zhangxin0214 <zhangxin0214@tal.com>
2022-09-13 17:33:51 +0800

    完成switch组件迁移

--- packages/render/element-plus/packages/switch/src/switch1.vue
+++ packages/render/element-plus/packages/switch/src/switch1.vue
@@ -1,23 +1,17 @@
 <template>
-	<widget-mask
-		:widget="widget"
-		:basicProp="widget.options.basic"
-		:advancedProp="widget.options.advanced"
-		:parent-widget="parentWidget"
-		:prop-key="propKey"
-		:rule-form="ruleForm"
-		:parent="parent"
-		:is-editor="isEditor"
-		:selected-widget="selectedWidget">
-
+	<el-form-item
+		:class="[selectedWidget?.id === widget?.id && isEditor?'select':'']"
+		:label="widget.options.basic.label.value"
+		:rules="widget.rules"
+		:prop="propKey"
+		:key="propKey">
 		<el-switch
 			v-model="widget.value"
 			:disabled="widget.options.basic.disabled.value"
 			@change="handleChangeEvent(props,ElMessage)"/>
-	</widget-mask>
+	</el-form-item>
 </template>
 <script setup name="switch1">
-import widgetMask from '../../common/widgetMask.vue';
 import { watch } from 'vue'
 import { ElMessage } from 'element-plus'
 import { handleChangeEvent } from '../../hooks/handleChangeEvent'
@@ -36,4 +30,12 @@ const props = defineProps([
 watchEvent(props,watch)
 </script>
 <style lang="scss" scoped>
+.hint {
+	font-size: 12px;
+	color: #9b9b9b;
+}
+// 选中样式
+.select {
+	outline: 1px solid $--color-primary;
+}
 </style>
