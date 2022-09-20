f0599acb0438f427e234775cee75badcd31ab51a
zhangxin0214 <zhangxin0214@tal.com>
2022-09-20 20:01:58 +0800

    初步完成栅格容器element-ui版本

--- packages/render/element-ui/packages/grid/src/grid.vue
+++ packages/render/element-ui/packages/grid/src/grid.vue
@@ -1,14 +1,11 @@
 <template>
-    <div class="grid">
-        <el-row
-			class="grid-container"
-			:style="`background-color: ${widget.options.basic.rowBackground.value}`"
-		>
-			<template
+	<el-row
+		class="grid-container"
+		:style="`background-color:${widget.options.basic.rowBackground.value}`">
+			<div
 				v-for="(colWidget, colIdx) in widget.options.advanced.cols"
-				:key="colIdx"
-			>
-				<Col
+				:key="colIdx">
+				<grid-col
 					:colWidget="colWidget"
 					:widget="widget"
 					:prop-key="propKey"
@@ -16,31 +13,30 @@
 					:is-editor="isEditor"
 					:rule-form="ruleForm[widget.ruleFormKey] || ruleForm"
 					:rule-form-key-type="widget.ruleFormKeyType"
-					:style="`height: ${widget.options.basic.colHeight.value}px;`"
+					:style="`height:${widget.options.basic.colHeight.value}px;`"
 				>
-					<slot 
-					 name="widgetChild"
-					 v-bind="{
-							colWidget, 
-							propKey,
-							ruleForm: ruleForm[widget.ruleFormKey] || ruleForm,
-							index:colIdx
-						}">
-					</slot>
-				</Col>
-			</template>
-		</el-row>
-    </div>
+				<slot 
+					name="widgetChild"
+					v-bind="{
+						colWidget, 
+						propKey,
+						ruleForm: ruleForm[widget.ruleFormKey] || ruleForm,
+						index:colIdx
+					}">
+				</slot>
+			</grid-col>
+			</div>
+	</el-row>
 </template>
 <script>
 	import mixins from '../../mixins/index.js'
-	import Col from './grid-col.vue'
+	import GridCol from './grid-col.vue'
 	export default {
-	  name: "fcGrid",
-	  mixins:[mixins],
-	  components: [
-	  	Col
-	  ]
+		name: "fcGrid",
+		mixins:[mixins],
+		components: {
+			GridCol
+		}
 	};
 </script>
 <style scope>
