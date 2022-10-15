561c814a6c5f877cea0b54bf9efb15b0ce36708f
zhangxin0214s <15110272646@163.com>
2022-10-15 23:53:52 +0800

    调整文件路径

--- packages/libs/element-plus/packages/renderer/src/index.vue
+++ packages/libs/element-plus/packages/renderer/src/index.vue
@@ -44,7 +44,7 @@ export default {
     import { onMounted, onBeforeMount, ref } from 'vue';
     import widgetMask from './widgetMask.vue'
     import containerMask from './containerMask.vue'
-    import { useRegisterEvent } from '../../../../libs/element-plus/packages/index.js'
+    import { useRegisterEvent } from '../../hooks/useRegisterEvent'
     const props = defineProps(['widgetList', 'formConfig', 'isEditor', 'widgetStore']);
     const ruleFormRef = ref(null);
     const submitForm = async () =>{
