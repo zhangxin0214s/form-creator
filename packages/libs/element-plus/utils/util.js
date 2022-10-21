ef3c928da70f541d3712754042b53e9ea0b68873
zhangxin0214s <15110272646@163.com>
2022-10-22 01:08:52 +0800

    完成各种查询方法

--- packages/libs/element-plus/utils/util.js
+++ packages/libs/element-plus/utils/util.js
@@ -1,6 +1,3 @@
-// import { widgetStore } from '@/store/index';
-
-// const _widgetStore = widgetStore();
 /**
  * 根据id查找相应元素
  * @param {*} id 
@@ -12,8 +9,12 @@ export function getWidgetById(id, widgetList) {
   let target = null;
   while (stack.length) {
     let node = stack.pop();
-    if (node.id === id) target = node;
-    if (target !== null) stack = [];
+    if (node.id === id) {
+      target = node;
+    }
+    if (target !== null) {
+      stack = [];
+    }
     if (target === null && ['fcGrid', 'fcTabs', 'fcCard'].indexOf(node.type) > -1) {
       node.options.advanced.cols.forEach((c) => {
         stack.push(...c.widgetList);
@@ -21,33 +22,6 @@ export function getWidgetById(id, widgetList) {
     }
   }
   return target;
-  // let target = widgetList.find(w=>w.id === id);
-  // if (!target) {
-  //   for (let i = 0; i < widgetList.length; i++) {
-  //     if (['fcGrid', 'fcTabs', 'fcCard'].indexOf(widgetList[i].type) > -1) {
-  //       const cols = widgetList[i].options.advanced.cols;
-  //       for (let k = 0; k < cols.length; k++) {
-  //         let w = getWidgetById(id, cols[k].widgetList);
-  //         if(w) return w;
-  //       }
-  //     }
-  //   }
-  // }
-  // return target
-  // let target = null;
-  // widgetList.forEach(widget => {
-  //   if (widget.id === id) {
-  //     target = widget
-  //   } else {
-  //     if (['fcGrid', 'fcTabs', 'fcCard'].indexOf(widget.type) > -1) {
-  //       const cols = widget.options.advanced.cols
-  //       cols.forEach(col => {
-  //         if (!target) getWidgetById(id, col.widgetList)
-  //       })
-  //     }
-  //   }
-  // })
-  // return target
 }
 
 /**
@@ -56,19 +30,41 @@ export function getWidgetById(id, widgetList) {
  * @param {*} widgetList 
  * @returns 
  */
-export function getWidgetByKey(key, widgetList, targets = []) {
-  widgetList.forEach(widget => {
-    if (widget.ruleFormKey === key) {
-      targets.push(widget)
-    } else {
-      if (['fcGrid', 'fcTabs', 'fcCard'].indexOf(widget.type) > -1) {
-        const cols = widget.options.advanced.cols
-        cols.forEach(col => {
-          getWidgetById(key, col.widgetList, targets)
-        })
-      }
+export function getWidgetsByKey(key, widgetList) {
+  let stack = [...widgetList];
+  let target = [];
+  while (stack.length) {
+    let node = stack.pop();
+    if (node.id === id) {
+      target.push(node);
     }
-  })
-  console.log(targets, "===查找到的元素===")
-  return targets
+    if (target !== null) {
+      stack = [];
+    }
+    if (target === null && ['fcGrid', 'fcTabs', 'fcCard'].indexOf(node.type) > -1) {
+      node.options.advanced.cols.forEach((c) => {
+        stack.push(...c.widgetList);
+      });
+    }
+  }
+  return target;
+}
+
+/**
+ * 根据customAttrName查找相应元素
+ * @param {*} attr  customAttrName
+ * @param {*} stage 
+ * @returns 
+ */
+export function getElementsByAttr(attr, stage) {
+  let stack = [stage];
+  let target = [];
+  while (stack.length) {
+    let node = stack.pop();
+    if (node.customAttrName === attr) {
+      target.push(node);
+    }
+    stack.push(...(node.children || []))
+  }
+  return target;
 }
--- test/index.js
+++ test/index.js
@@ -1,3 +1,153 @@
-const _widgetStore = fc.widgetStore();
+function getElementsByAttr(attr, stage) {
+    let stack = [stage];
+    let target = [];
+    while (stack.length) {
+      let node = stack.pop();
+      if (node.customAttrName === attr) {
+        target.push(node);
+      }
+      stack.push(...(node.children || []))
+    }
+    return target;
+}
 
-console.log(_widgetStore.formConfig)
+var a =  {
+    "originWidth": 1920,
+    "originHeight": 1080,
+    "windowPercent": "16:9",
+    "_backgroundImgId": 18,
+    "answer": [],
+    "dom": [],
+    "type": 1,
+    "conName": "Stage",
+    "id": 1,
+    "name": "stage",
+    "alias": "StageAlias",
+    "visible": true,
+    "interactive": true,
+    "props": {
+        "canBeDisabled": true,
+        "disabled": false
+    },
+    "edit": {
+        "select": true,
+        "delete": true,
+        "copy": true,
+        "editText": true,
+        "changeFile": true,
+        "move": true
+    },
+    "componentType": "",
+    "questionDescribe": {},
+    "content": {
+        "click": "",
+        "hover": "",
+        "default": 18
+    },
+    "customAttrName": "START",
+    "alpha": 1,
+    "transform": [
+        0,
+        0,
+        1,
+        1,
+        0,
+        0,
+        0,
+        0,
+        0
+    ],
+    "children": [
+        {
+            "type": 4,
+            "conName": "Sprite",
+            "id": 3,
+            "name": "图片_3",
+            "alias": "图片_3",
+            "visible": true,
+            "interactive": true,
+            "props": {
+                "startAudio": {
+                    "id": 206,
+                    "isResourceId": true
+                }
+            },
+            "edit": {
+                "select": true,
+                "delete": false,
+                "copy": false,
+                "editText": true,
+                "changeFile": true,
+                "move": true
+            },
+            "componentType": "",
+            "questionDescribe": {},
+            "content": {
+                "click": "",
+                "hover": "",
+                "default": 20
+            },
+            "customAttrName": "audioBtn",
+            "alpha": 1,
+            "transform": [
+                823.03,
+                766.57,
+                1,
+                1,
+                0,
+                0,
+                0,
+                0,
+                0
+            ],
+            "children": []
+        },
+        {
+            "type": 15,
+            "conName": "Button",
+            "id": 4,
+            "name": "按钮p_4",
+            "alias": "按钮p_4",
+            "visible": true,
+            "interactive": true,
+            "props": {
+                "audioBgDefault": {
+                    "isResourceId": true,
+                    "id": 197,
+                    "fileName": "上传成功"
+                }
+            },
+            "edit": {
+                "select": true,
+                "delete": true,
+                "copy": true,
+                "editText": true,
+                "changeFile": true,
+                "move": true
+            },
+            "componentType": "",
+            "questionDescribe": {},
+            "content": {
+                "default": 22,
+                "hover": "",
+                "click": 24
+            },
+            "customAttrName": "audioBtn",
+            "alpha": 1,
+            "transform": [
+                1720.33,
+                65.37,
+                1,
+                1,
+                0,
+                0,
+                0,
+                0,
+                0
+            ],
+            "children": []
+        }
+    ]
+}
+
+getElementsByAttr('audioBtn', a)
