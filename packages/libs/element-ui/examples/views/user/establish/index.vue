eefd8975592e040b87022ae58cae5c8be98535ca
zhangxin0214 <zhangxin0214@tal.com>
2022-11-05 19:41:48 +0800

    opt code

--- packages/libs/element-ui/examples/views/user/establish/index.vue
+++ packages/libs/element-ui/examples/views/user/establish/index.vue
@@ -1,5 +1,23 @@
 <template>
    <div>
-      
+      <fc-renderer
+        :widgetStore="widgetStore"
+        :widgetList="widgetList"
+        :formConfig="formConfig"
+    ></fc-renderer>
    </div>
 </template>
+<script>
+import formJson from './index.json';
+console.log(formJson,"===formJson===");
+export default {
+  name: "App",
+  data() {
+      return {
+        widgetStore: null,
+        widgetList: formJson.widgetList,
+        formConfig: formJson.formConfig
+      }
+  }
+};
+</script>
