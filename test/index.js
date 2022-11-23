a9d9c3ceeb3219d90c523bbdc3c9670172fbbd3c
zhangxin0214s <15110272646@163.com>
2022-11-23 21:19:29 +0800

    opt code

--- test/index.js
+++ test/index.js
@@ -11,3 +11,32 @@ fc.request['user/account']({
         user_email = res.data.email;
     }
 })
+
+
+if (!fc.props.ruleFormRef) return
+fc.props.ruleFormRef.validate((valid, fields) = >{
+    if (valid) { 
+        console.log('submit!') 
+        console.log(fc.props.ruleForm, \"===ruleForm===\")    
+        fc.request['user/saveUserInfo'](fc.props.ruleForm).then(res =>{      
+            if(res.code === 200){        
+                fc.ElMessage({
+                    message: '保存成功',          
+                    type: 'success',          
+                    duration: 1000        
+                })      
+            }else{        
+                fc.ElMessage({          
+                    message: res.msg,          
+                    type: 'error',          
+                    duration: 1000        
+                })      
+            }    
+        })  
+    } else {   
+         console.log('error submit!', fields)    
+         fc.ElMessage({      
+            message: '有必填项未填写，或填写错误，请检查',      
+            type: 'error',      duration: 1000    
+        })  
+    }})
